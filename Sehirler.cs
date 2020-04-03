7b056cd73b98d75d1ad21aecb8c777104f6c77ba
tolunayesergun <tolunay50@gmail.com>
2020-04-03 19:58:03 +0300

    Graf metin belgesinden çekilecek şekilde ayarlandı, optimizasyonlar yapıldı

--- Sehirler.cs
+++ Sehirler.cs
@@ -8,14 +8,27 @@ namespace StorkShipping
 {
     class Sehirler
     {
-             public static string[] SehirAd = { " ","Adana", "Adıyaman", "Afyon", "Ağrı", "Amasya", "Ankara", "Antalya", "Artvin", "Aydın",
+        public static string[] SehirAd = { " ","Adana", "Adıyaman", "Afyon", "Ağrı", "Amasya", "Ankara", "Antalya", "Artvin", "Aydın",
             "Balıkesir", "Bilecik", "Bingöl", "Bitlis", "Bolu", "Burdur", "Bursa", "Çanakkale", "Çankırı", "Çorum", "Denizli",
-            "Diyarbakır", "Edirne", "Elazığ", "Ezincan", "Erzurum", "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane", "Hakkari",
+            "Diyarbakır", "Edirne", "Elazığ", "Erzincan", "Erzurum", "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane", "Hakkari",
             "Hatay", "Isparta", "Mersin", "İstanbul", "İzmir", "Kars", "Kastamonu", "Kayseri", "Kırklareli", "Kırşehir", "Kocaeli",
             "Konya", "Kütahya", "Malatya", "Manisa", "Kahramanmaraş", "Mardin", "Muğla", "Muş", "Nevşehir", "Niğde", "Ordu", "Rize",
             "Sakarya", "Samsun", "Siirt", "Sinop", "Sivas", "Tekirdağ", "Tokat", "Trabzon", "Tunceli", "Şanlıurfa", "Uşak", "Van", "Yozgat",
             "Zonguldak", "Aksaray", "Bayburt", "Karaman", "Kırıkkale", "Batman", "Şırnak", "Bartın", "Ardahan", "Iğdır", "Yalova", "Karabük",
             "Kilis", "Osmaniye", "Düzce" };
+
+        public static string[] CityName = { " ", "Adana", "Adiyaman", "Afyonkarahisar", "Agri", "Amasya", "Ankara", "Antalya", "Artvin", "Aydin",
+            "Balikesir", "Bilecik", "Bingol", "Bitlis", "Bolu", "Burdur", "Bursa", "Canakkale", "Cankiri", "Corum", "Denizli",
+            "Diyarbakir", "Edirne", "Elazig", "Erzincan", "Erzurum", "Eskisehir", "Gaziantep", "Giresun", "Gumushane", "Hakkari",
+            "Hatay", "Isparta", "Mersin", "Istanbul", "Izmir", "Kars", "Kastamonu", "Kayseri", "Kirklareli", "Kirsehir", "Kocaeli",
+            "Konya", "Kutahya", "Malatya", "Manisa", "Kahramanmaras", "Mardin", "Mugla", "Mus", "Nevsehir", "Nigde", "Ordu", "Rize",
+            "Sakarya", "Samsun", "Siirt", "Sinop", "Sivas", "Tekirdag", "Tokat", "Trabzon", "Tunceli", "Sanliurfa", "Usak", "Van", "Yozgat",
+            "Zonguldak", "Aksaray", "Bayburt", "Karaman", "Kirikkale", "Batman", "Sirnak", "Bartin", "Ardahan", "Igdir", "Yalova", "Karabuk",
+            "Kilis", "Osmaniye", "Duzce" };
+   
+
+        public static int[,] graf=new int[81,81];
+
         public static int SehirPlakaBul(string Sehir)
         {
             int plaka=0;
@@ -27,7 +40,21 @@ namespace StorkShipping
                     plaka = i;
                 }
             }
+            if (plaka == 0)plaka=FindCityName(Sehir);
 
+            return plaka;
+        }
+        public static int FindCityName(string Sehir)
+        {
+            int plaka = 0;
+
+            for (int i = 0; i < CityName.Length; i++)
+            {
+                if (CityName[i].ToUpper() == Sehir.ToUpper())
+                {
+                    plaka = i;
+                }
+            }
             return plaka;
         }
     }
